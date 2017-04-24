0a06e44b6751e37928dacfbe96c31e95df8f4358
Samael Infernus <Samael Infernus>
2017-04-24 13:32:22 -0400

    rake refactor

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -26,13 +26,16 @@ if [ "$1" = 'ofn' ]; then
     sed -e "s#production:#${RAILS_ENV}:#" -e "s#.*adapter:.*#  adapter: postgresql#" -e "s#.*username:.*#  username: ${OFN_DB_USER}#" -e "s#.*password:.*#  password: ${OFN_DB_PASS}#" -e "s#.*database:.*#  database: ${OFN_DB}\n  host: ${OFN_DB_HOST}#" < ${OFN_DIR}/config/database.yml.pkgr > ${OFN_DIR}/config/database.yml
     cd ${OFN_DIR}
     # populate database
-    echo "===> Running db:drop, db:create, db:schema:load, db:seed..."
-    # bundle exec rake db:setup
+    echo "===> Running db:drop..."
     bundle exec rake db:drop
+    echo "===> Running db:create..."
     bundle exec rake db:create
+    echo "===> Running db:schema:load..."
     bundle exec rake db:schema:load
+    echo "===> Running db:migrate..."
     bundle exec rake db:migrate
-    # bundle exec rake db:seed
+    echo "===> Running db:seed..."
+    bundle exec rake db:seed
 
 
     # assets precompile
