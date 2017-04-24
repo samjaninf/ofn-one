f847cfca923b572a5472a69d1670ec6b480d9425
Samael Infernus <Samael Infernus>
2017-04-24 14:24:35 -0400

    rake refactor 4

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -36,8 +36,6 @@ if [ "$1" = 'ofn' ]; then
     bundle exec rake db:migrate
     echo "===> Running db:seed..."
     bundle exec rake db:seed
-    echo "===> Running dev..."
-    bundle exec rake load_sample_data
 
 
     # assets precompile
