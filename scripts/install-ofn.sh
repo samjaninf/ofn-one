28d903a7a7dc384e5c4666c987b5641625d82447
Samael Infernus <Samael Infernus>
2017-04-27 13:55:29 -0400

    Icons and proper reverse proxy

--- scripts/install-ofn.sh
+++ scripts/install-ofn.sh
@@ -26,7 +26,7 @@ update-ca-certificates -f
 #apt-get --no-install-recommends -y install elasticsearch
 #cd /usr/share/elasticsearch && bin/elasticsearch-plugin install mapper-attachments
 #service elasticsearch start
-
+sed -i -e "s#.*tcp_nodelay on.*#        tcp_nodelay off;#" -e "s#.*\# gzip_vary on.*#        gzip_vary on;#" -e "s#.*\# gzip_proxied.*#        gzip_proxied any;#" -e "s#.*\# gzip_http_version.*#        gzip_http_version 1.1;#" -e "s#.*\# gzip_types text/plain text/css application/json application/javascriptt text/xml application/xml application/xml+rss text/javascript;.*#        gzip_types text/plain text/xml text/css text/comma-separated-values text/javascript application/x-javascript application/atom+xml;#" /etc/nginx/nginx.conf
 # create zammad user
 #useradd -M -d /opt/ofn -s /bin/bash ofn
 
