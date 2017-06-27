46a42649a80c9f8049c8e5fff8ec7a7d03dbe1ba
Jazzy Jones <samjaninf@users.noreply.github.com>
2017-06-26 23:23:22 -0400

    added delayed_job

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -3,24 +3,6 @@
 set -e
 
 if [ "$1" = 'ofn' ]; then
-
-  # starting services
-  #service postgresql start
-  #service elasticsearch start
-
-  # wait for postgres processe coming up
-  #until su - postgres -c 'psql -c "select version()"' &> /dev/null; do
-  #until
-  #  echo "waiting for postgres to be ready..."
-  #  sleep 2
-  #done
-
-
-  #until $(mysql -s -N -e -u${OFN_DB_USER} -p${OFN_DB_PASS} -h ${OFN_DB_HOST} "SELECT schema_name FROM information_schema.schemata WHERE SCHEMA_NAME = '${OFN_DB}'"); do
-  #  echo "=> Waiting for MariaDB to be ready..."
-  #done
-  #export tableExists=$(mysql -s -N -e -u${OFN_DB_USER} -p${OFN_DB_PASS} -h ${OFN_DB_HOST} "SELECT * FROM information_schema.tables WHERE table_schema = '${OFN_DB}' AND table_name = 'users'")
-  # if [[ -z "${tableExists}" ]]; then
     export PGPASSWORD=$OFN_DB_PASS
     export rakeSecret=$(rake secret)
     echo "===> Configuring Openfoodnetwork for production please wait..."
@@ -31,7 +13,6 @@ if [ "$1" = 'ofn' ]; then
       echo "===> Running db:drop..."
       bundle exec rake db:drop
       echo "===> Running db:create..."
-      # psql -h postgresql -U $OFN_DB_USER -q -d $OFN_DB -c 'SELECT 1;' || bundle exec rake db:create
       bundle exec rake db:create
       echo "===> Running db:schema:load..."
       bundle exec rake db:schema:load || echo "<== Schema already loaded..."
@@ -98,15 +79,16 @@ EOF
     bundle exec unicorn -p 3000 -c config/unicorn.rb -E ${RAILS_ENV} &>> ${OFN_DIR}/log/ofn.log &
   fi
 
-  # wait for zammad processe coming up
+  # wait for openfoodnetwork processe coming up
   until (echo > /dev/tcp/localhost/3000) &> /dev/null; do
     echo "==> waiting for openfoodnetwork to be ready..."
     sleep 10
   done
 
+  script/delayed_job -n 2 start
 
   # show url
-  echo -e "===> \Openfoodnetwork is ready! Visit the url in your browser to configure!"
+  echo -e "===> Openfoodnetwork is ready! Visit the url in your browser to configure!"
 
   # run shell
   tail -f ${OFN_DIR}/log/ofn.log
